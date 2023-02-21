c27b574ffec51e8f67604e4ff3b37734e347754c
EdwinSch <edwinschutjes@gmail.com>
2023-02-21 18:39:57 +0100

    add unique id generator logic (hardcode)

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 /* ---- TARGETS && INITIALIZERS ---- */
+const form = document.getElementById("input-form");
+const output = document.querySelector(".id-output");
+const characters = "abcdefghijklmnopqrstuvwxyz".split("");
+const numbers = Array.from(Array(10).keys());
 
 /* ---- FUNCTIONS ---- */
 
-console.log("hello world");
+const mapper = characters.concat(numbers);
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  for (let i = 0; i < 20; i++) {
+    let id = "";
+    for (let i = 0; i < 10; i++) {
+      id += mapper[getInt()];
+    }
+    const line = document.createElement("p");
+    line.innerHTML = `<p>${id}</p>`;
+    output.appendChild(line);
+  }
+});
+
+function getInt() {
+  return Math.floor(Math.random() * mapper.length);
+}
